dc8ddbd4a75ecd01eef1ae9b6e043e7bc752e009
Ahmed Magdy <ahmedahmedmagdy491@gmail.com>
2020-07-01 10:10:20 -0700

    modifyied

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 new hoverEffect({
     parent : document.querySelector('.distortion'),
     intensity : 0.2,
-    image1: "dw1.jpg",
-    image2: "dw2.jpg",
-    displacementImage : "dss.png"
+    image1: "images/dw1.jpg",
+    image2: "images/dw2.jpg",
+    displacementImage : "images/dss.png"
 });
